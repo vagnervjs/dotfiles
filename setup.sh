59e54b552c8c107c7472676635d93a5decedd6e1
Vagner Santana <vagnervjs@gmail.com>
2015-04-16 21:44:53 -0300

    Add Sublime Setup & Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,22 @@
-cd ~
-ln -s ~/Dropbox/.dotfiles/.zshrc
-ln -s ~/Dropbox/.dotfiles/.gitconfig
-ln -s ~/Dropbox/.dotfiles/.gitignore_global
+# .dotifles setup
 
+# ~/ Sync
+cd ~
+rm -rf .zshrc
+ln -s $HOME/Dropbox/.dotfiles/.zshrc
+
+rm -rf .gitconfig
+ln -s $HOME/Dropbox/.dotfiles/.gitconfig
+
+rm -rf .gitignore_global
+ln -s $HOME/Dropbox/.dotfiles/.gitignore_global
+
+# Sublime Sync
 cd ~/Library/Application\ Support/Sublime\ Text\ 3/
 rm -rf Installed\ Packages
-ln -s ~/Dropbox/Sublime/Installed\ Packages
+ln -s $HOME/Dropbox/.dotfiles/Sublime/Installed\ Packages
 
 rm -rf Packages
-ln -s ~/Dropbox/Sublime/Packages
+ln -s $HOME/Dropbox/.dotfiles/Sublime/Packages
+
+echo "Dotfiles setup done!"
